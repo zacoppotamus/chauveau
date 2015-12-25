8c0f68be4982076dfe1129aff04e7bfcffe66870
Zac Ioannidis <zacoppotamus@gmail.com>
2015-12-25 13:46:32 +0200

    Initial commit

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('sqlChauveauApp.admin', [
+  'sqlChauveauApp.auth',
+  'ui.router'
+]);
---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('sqlChauveauApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
